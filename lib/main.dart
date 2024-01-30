8eaba1d379f559d672cb86d6f491177ec00df715
unclegenson <unclegenson@gmail.com>
2024-01-30 13:46:20 +0330

    cjange some little things

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'YekanBakh',
+        // NABEGHEHA.COM
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/player_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
+
+class MyVideoPlayerPage extends StatefulWidget {
+  const MyVideoPlayerPage({super.key});
+
+  @override
+  State<MyVideoPlayerPage> createState() => _MyVideoPlayerPageState();
+}
+
+class _MyVideoPlayerPageState extends State<MyVideoPlayerPage> {
+  late VideoPlayerController videoPlayerController;
+  ChewieController? chewieController;
+
+  @override
+  void initState() {
+    videoPlayerController =
+        VideoPlayerController.asset('assets/videos/vid-1.mp4');
+
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: true,
+      looping: true,
+      showControls: false,
+      showControlsOnInitialize: false,
+      aspectRatio: 0.5,
+      optionsTranslation: OptionsTranslation(
+        playbackSpeedButtonText: 'سرعت ویدیو',
+        subtitlesButtonText: 'زیرنویس ویدیو',
+        cancelButtonText: 'انصراف',
+      ),
+    );
+    // NABEGHEHA.COM
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    chewieController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: chewieController != null
+          ? SafeArea(
+              child: Chewie(controller: chewieController!),
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+}
